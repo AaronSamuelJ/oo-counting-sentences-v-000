de901bd124122d524a77cb526a45d8e5365a87da
Aaron Samuel <aasamuelj@gmail.com>
2017-09-28 22:43:45 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,8 +1,11 @@
 class String
   def sentence?
-    self.end_with?(".") 
+    self.end_with?(".")
   end
   def question?
     self.end_with?("?")
+  end
+  def exclamation?
+    self.end_with?("!")
   end
 end
