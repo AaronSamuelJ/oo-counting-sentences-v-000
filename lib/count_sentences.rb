fceafbb401305287ca5d1a36dbd81f4081524d7e
Aaron Samuel <aasamuelj@gmail.com>
2017-09-28 22:46:56 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class String
   def sentence?
     self.end_with?(".")
@@ -9,6 +10,7 @@ class String
     self.end_with?("!")
   end
   def count_sentences
+    binding.pry
     self.split(" ").size
   end
 end
